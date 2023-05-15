16e31399c7dc3739e89aeb2237ad111688584cf9
SeiyaMaruyama <s256255@wakayama-u.ac.jp>
2023-05-15 23:21:18 +0900

    課題完成

--- ggsample04.cpp
+++ ggsample04.cpp
@@ -23,6 +23,70 @@
 // アニメーションの周期（秒）
 constexpr auto cycle{ 5.0 };
 
+//qmake
+void findQuaternion(float* q, float x, float y, float z, float a) {
+  float n = x * x + y * y + z * z;
+
+  if(n != 0.0) {
+    float s = sin(a *= 0.5f) / sqrt(n);
+
+    q[0] = x * s;
+    q[1] = y * s;
+    q[2] = z * s;
+    q[3] = cos(a);
+  }
+}
+
+//slerp
+void interpolateSphericalLinear(float* p, const float* q, const float* r, float t) {
+  float qr = q[0] * r[0] + q[1] * r[1] + q[2] * r[2] + q[3] * r[3];
+  float ss = 1.0f - qr * qr;
+
+  if(ss == 0.0) {
+    p[0] = q[0];
+    p[1] = q[1];
+    p[2] = q[2];
+    p[3] = q[3];
+  }
+  else {
+    float sp = sqrt(ss);
+    float ph = acos(qr);
+    float pt = ph * t;
+    float t1 = sin(pt) / sp;
+    float t0 = sin(ph - pt) / sp;
+
+    p[0] = q[0] * t0 + r[0] * t1;
+    p[1] = q[1] * t0 + r[1] * t1;
+    p[2] = q[2] * t0 + r[2] * t1;
+    p[3] = q[3] * t0 + r[3] * t1;
+  }
+}
+
+//qrot
+void rotTransMat(GLfloat* m, const float* q) {
+  float xx = q[0] * q[0] * 2.0f;
+  float yy = q[1] * q[1] * 2.0f;
+  float zz = q[2] * q[2] * 2.0f;
+  float xy = q[0] * q[1] * 2.0f;
+  float yz = q[1] * q[2] * 2.0f;
+  float zx = q[2] * q[0] * 2.0f;
+  float xw = q[0] * q[3] * 2.0f;
+  float yw = q[1] * q[3] * 2.0f;
+  float zw = q[2] * q[3] * 2.0f;
+
+  m[0] = 1.0f - yy - zz;
+  m[1] = xy + zw;
+  m[2] = zx - yw;
+  m[4] = xy - zw;
+  m[5] = 1.0f - zz - xx;
+  m[6] = yz + xw;
+  m[8] = zx + yw;
+  m[9] = yz - xw;
+  m[10] = 1.0f - xx - yy;
+  m[15] = 1.0f;
+  m[3] = m[7] = m[11] = m[12] = m[13] = m[14] = 0.0f;
+}
+
 //
 // アプリケーション本体
 //
@@ -123,8 +187,14 @@ int GgApp::main(int argc, const char* const* argv)
 
     // 時刻 t にもとづく回転アニメーション
     GLfloat mr[16];                   // 回転の変換行列
-    // 【宿題】ここを解答してください（下の loadIdentity() を置き換えてください）
-    loadIdentity(mr);
+    float q0[4], q1[4], qt[4];
+    //qmake
+    findQuaternion(q0, 1.0f, 0.0f, 0.0f, 1.0f);
+    findQuaternion(q1, 0.0f, 0.0f, 1.0f, 2.0f);
+    //slerp
+    interpolateSphericalLinear(qt, q0, q1, t);
+    //qrot
+    rotTransMat(mr, qt);
 
     // 時刻 t にもとづく平行移動アニメーション
     float location[3];                // 現在位置
